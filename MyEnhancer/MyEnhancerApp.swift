7893c7d2e5216a581f378877148e5f79833bcb08
Ethan <zhangyuxin85@gmail.com>
2025-02-05 15:38:40 -0800

    Initial Commit

---
+++ MyEnhancer/MyEnhancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEnhancerApp.swift
+//  MyEnhancer
+//
+//  Created by Ethan on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct MyEnhancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
